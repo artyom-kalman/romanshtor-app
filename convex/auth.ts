56869219461df3644d714ccfa96d4b0a031a90d2
artyom-kalman <p.a.kalashnikov@ya.ru>
2026-02-08 20:35:43 +0900

    Update resend config

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,5 +2,5 @@ import Resend from "@auth/core/providers/resend";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Resend],
+  providers: [Resend({ from: "noreply@rimskiestory.ru" })],
 });
